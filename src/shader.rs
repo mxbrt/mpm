e66dd9863c61ef8933db99e6f9be48950a2cd46d
Max Breitenfeldt <git@mxbr.me>
2020-10-23 23:05:02 +0200

    Shader: add include callback

--- src/shader.rs
+++ src/shader.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 
-use shaderc::{CompilationArtifact, Compiler, ShaderKind};
+use shaderc::{
+    CompilationArtifact, CompileOptions, Compiler, IncludeType, ResolvedInclude, ShaderKind,
+};
 
 pub fn load(path: &str, device: &wgpu::Device) -> wgpu::ShaderModule {
     let spirv = match compile_glsl(path) {
@@ -10,8 +12,25 @@ pub fn load(path: &str, device: &wgpu::Device) -> wgpu::ShaderModule {
     device.create_shader_module(wgpu::ShaderModuleSource::SpirV(spirv.as_binary().into()))
 }
 
+fn include_callback(
+    path: &str,
+    _: IncludeType,
+    _: &str,
+    _: usize,
+) -> Result<ResolvedInclude, String> {
+    match std::fs::read_to_string(path) {
+        Ok(source) => Ok(ResolvedInclude {
+            resolved_name: String::from(path),
+            content: source,
+        }),
+        Err(e) => Err(format!("Failed to load included file {}\n{}", path, e)),
+    }
+}
+
 fn compile_glsl(path: &str) -> Result<CompilationArtifact, Box<dyn Error>> {
     let mut compiler = Compiler::new().unwrap();
+    let mut options = CompileOptions::new().unwrap();
+    options.set_include_callback(include_callback);
     let _path = std::path::Path::new(path);
     let extension = _path.extension().unwrap().to_str().unwrap();
     let shader_kind = match extension {
@@ -21,6 +40,6 @@ fn compile_glsl(path: &str) -> Result<CompilationArtifact, Box<dyn Error>> {
         _ => return Err(format!("Invalid shader extension {}", extension).into()),
     };
     let source = std::fs::read_to_string(path)?;
-    let spirv = compiler.compile_into_spirv(&source, shader_kind, path, "main", None)?;
+    let spirv = compiler.compile_into_spirv(&source, shader_kind, path, "main", Some(&options))?;
     Ok(spirv)
 }
